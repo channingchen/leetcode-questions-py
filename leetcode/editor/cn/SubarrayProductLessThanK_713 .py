715895cd3dbfe4ca6585dbfe053393b08e33cdca
channingchen <jslygcm@qq.com>
2021-11-23 19:12:05 +0800

    hw

--- leetcode/editor/cn/SubarrayProductLessThanK_713 .py
+++ leetcode/editor/cn/SubarrayProductLessThanK_713 .py
@@ -32,11 +32,36 @@
 #  👍 312 👎 0
 
 
+from typing import List
+
+
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
+        mem_dict = {}
+        solution = 0
+        for n in range(len(nums)):
+            if nums[n] < k:
+                solution += 1
+            mem_dict[str(n) + "_1"] = nums[n]
+
+        for w in range(2, len(nums) + 1):
+            i = 0
+            while i <= len(nums) - w:
+                key=str(i) + "_" + str(w - 1)
+                if mem_dict.has_key(key):
+                if r and r < k:
+                    r *= nums[i + w - 1]
+                    if r < k:
+                        # print(nums[i:i + w])
+                        solution += 1
+                        mem_dict[str(i) + "_" + str(w)] = r
+                i += 1
+        return solution
+
+
 # leetcode submit region end(Prohibit modification and deletion)
 
 
 if __name__ == '__main__':
-    Solution().
+    print(Solution().numSubarrayProductLessThanK([10, 9, 10, 4, 3, 8, 3, 3, 6, 2, 10, 10, 9, 3], 19))
